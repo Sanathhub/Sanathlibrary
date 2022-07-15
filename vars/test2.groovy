36465816ae9cae95334ca878d0c9207c52eb2391
Sanathhub <108620787+Sanathhub@users.noreply.github.com>
2022-07-15 23:35:08 +0530

    Update test2.groovy

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -2,3 +2,11 @@ def contdownload(jobname)
 {
   git "https://github.com/Sanathhub/${jobname}.git"
 }
+def contbuild()
+{
+  sh "mvn package"
+}
+def contdeploy()
+{
+  deploy adapters: [tomcat9(credentialsId: '5242c1fb-e4fd-448e-abe1-dc9187894af3', path: '', url: 'http://172.31.90.66:8080')], contextPath: 'testingapp', war: '**/*.war'
+}
